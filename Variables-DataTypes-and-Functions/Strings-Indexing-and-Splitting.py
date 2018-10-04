193e7c8de3982bd1d1cab312dd63c18ca66699d6
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-10-04 14:46:19 +0530

    Indexing and Splitting Strings

---
+++ Variables-DataTypes-and-Functions/Strings-Indexing-and-Splitting.py
@@ -0,0 +1,15 @@
+'''
+Print the positive and negative indices of the string and split the first two characters of the string and print.
+'''
+
+a = "Hi There!"
+'''Indexing [0 to 8] or [-9 to -1]'''
+print("Index 0",a[0])
+print("Index 8",a[8])
+print("Index -1",a[-1])
+print("Index -9",a[-9])
+'''Splitting [0 to 2] or [-9 to -7]'''
+print("Splitting:",a[0:2])
+print("Splitting:",a[:2])
+print("Splitting:",a[-9:-7])
+print("Splitting:",a[:-7])
