dfd06b619c8f0dabd90bb6db4648e5ecc38d64f9
Nishant Nirmal <nirmalnishant645@gmail.com>
2018-10-04 14:30:23 +0530

    Taking Integer Input and doing Arithmetic Operations

--- Variables-DataTypes-and-Functions/Numbers.py
+++ Variables-DataTypes-and-Functions/Numbers.py
@@ -1,3 +1,7 @@
+'''
+Take input from the user of his/her age and print the new age after 50 years.
+'''
+
 age = input("Enter your age: ")
 new_age = int(age) + 50
 print("Age after 50 years:",new_age)
