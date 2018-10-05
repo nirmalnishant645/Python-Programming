bb1cab78ec10b88d9751ea4440c97fc9b4e551ce
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-05 13:56:30 +0530

    Introduction of Tuples

---
+++ Variables-DataTypes-and-Functions/Tuples.py
@@ -0,0 +1,7 @@
+'''
+Make a tuple and print the last item.
+'''
+tuple = ("Hello", 3, 4.6)
+print(tuple)
+print("Data Type of an item in Tuple:",type(tuple[2]))
+print("Print the last element of the tuple:",tuple[-1])
