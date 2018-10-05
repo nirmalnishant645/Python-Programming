57b3a57fad61398b42b0e3c845d19c5d2fcd42c7
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-05 14:19:42 +0530

    Introduction to Dictionaries

---
+++ Variables-DataTypes-and-Functions/Dictionaries.py
@@ -0,0 +1,9 @@
+'''
+Make a dictionary and access its items
+'''
+a = {"First_Name":"Nishant", "Last_Name":"Nirmal", "Cities":("Patna", "Kolkata", "New Delhi")}
+'''Accesing item using Key'''
+print(a["First_Name"])
+print(a["Cities"])
+'''Accesing a nested item (Item inside a List or Tuple)'''
+print(a["Cities"][2])
