4f60cf3f9a59c414406df9d807cd695f6c3e504f
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-11 09:36:28 +0530

    Length of the String

---
+++ Functions-and-Conditionals/Function-Exercise-02.py
@@ -0,0 +1,8 @@
+'''
+Create a function that takes any string as argument and returns the length of that string.
+'''
+def string_length(mystring):
+    return len(mystring)
+
+mystring = input("Enter a string: ")
+print(string_length(mystring))
