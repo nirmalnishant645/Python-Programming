6f624813329d478e9cb3bd78808af255b3513db2
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-11 10:23:59 +0530

    Length of Strings and Error for Integers

--- Functions-and-Conditionals/Functions-and-Conditional-Exercise-01.py
+++ Functions-and-Conditionals/Functions-and-Conditional-Exercise-01.py
@@ -6,10 +6,16 @@ string_length(10)
 that would generate an error since the len() function doesn't work for integers.
 
 Modify the function so that if an integer is passed as an input, the function should output a message like "Sorry integers don't have length".
+Also, add the conditional block so that floats are counted too.
 '''
-
 def string_length(mystring):
-    if type(mystring) == int:
+     if type(mystring) == int:
          return "Sorry, integers don't have length"
+     elif type(mystring) == float:
+         return "Sorry, floats don't have length"
      else:
          return len(mystring)
+
+print(string_length(5))
+print(string_length(50.5))
+print(string_length("Code"))
