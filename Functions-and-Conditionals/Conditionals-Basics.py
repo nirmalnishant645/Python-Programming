41efaee7a53b7b139ec180a0630279d774e3f179
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-11 09:47:33 +0530

    If Else : Check if a number is less than, equal to or greater than 5

---
+++ Functions-and-Conditionals/Conditionals-Basics.py
@@ -0,0 +1,11 @@
+'''
+Program that checks if the input number is less than, equal to or greater than 5
+'''
+a = int(input("Enter a number from 0-10: "))
+
+if a < 5:
+    print("Less than 5")
+elif a == 5:
+    print("Equal to 5")
+else:
+    print("Greater than 5")
