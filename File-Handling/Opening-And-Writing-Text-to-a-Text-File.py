aae42520b261071e37118e02faa4647e1263f0b9
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-15 13:21:10 +0530

    Opening or Creating a Text File and writing inside it

--- File-Handling/Opening-And-Writing-Text-to-a-Text-File.py
+++ File-Handling/Opening-And-Writing-Text-to-a-Text-File.py
@@ -1,3 +1,6 @@
+'''
+Write some code that creates a text file and writes the items of list to that text file.
+'''
 file = open("example1.txt",'w')
 l=["Line number 1","Line number 2","Line number 3"]
 for item in l:
