e617b21f93d8ea6e95ef1bb53217447439350aab
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-10-16 09:08:36 +0530

    Create a file and name it with current time

---
+++ Dates-And-Times/Naming-File-with-Current-Time.py
@@ -0,0 +1,10 @@
+'''
+Create a new file and name it with the current time
+'''
+import datetime
+filename = datetime.datetime.now()
+def create_file():
+    with open(filename.strftime("%Y-%m-%d-%H-%M"),"w") as file:
+        file.write("")
+
+create_file()
