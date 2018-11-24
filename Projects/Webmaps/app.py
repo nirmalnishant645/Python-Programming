942c914079b168a5584629f60803ec202a8dceb7
nirmalnishant645 <nirmalnishant645@gmail.com>
2018-11-24 14:55:25 +0530

    Add Marker to Map

--- Projects/Webmaps/app.py
+++ Projects/Webmaps/app.py
@@ -1,3 +1,9 @@
-import folium
-map = folium.Map(location=[28.6, 77.1])
-map.save("Map1.html")
+import folium #import folium
+map = folium.Map(location=[28.6, 77.1], zoom_start=6, tiles="Mapbox Bright") #Open Street Map
+
+fg = folium.FeatureGroup(name="My Map") #Making a Feature Group
+fg.add_child(folium.Marker(location=[28.6, 77.1], popup="Marker", icon=folium.Icon(color='green'))) #Add marker to the Feature Group as a child
+
+map.add_child(fg) #Make Featuer Group child of Map
+
+map.save("Map1.html") #Save the HTML file of Map
