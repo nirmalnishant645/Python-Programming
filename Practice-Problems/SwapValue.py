8698816c3afc9df3ef97d2ede7306cc5fc576fbe
Nishant Nirmal <nirmalnishant645@gmail.com>
2019-09-28 11:06:37 +0530

    Swap Value

---
+++ Practice-Problems/SwapValue.py
@@ -0,0 +1,9 @@
+'''
+Program to compute Simple Interest and display the result
+'''a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print ("a = ",a)
+print ("b = ",b)
+a,b = b,a
+print ("Swapped a = ",a)
+print ("Swapped b = ",b)
