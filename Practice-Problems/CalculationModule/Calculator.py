128d169f115038f340f438a67f8d634071c97245
nirmalnishant645 <nirmalnishant645@gmail.com>
2019-10-07 17:50:27 +0530

    Simple Calculator using user module

---
+++ Practice-Problems/CalculationModule/Calculator.py
@@ -0,0 +1,9 @@
+import calculation
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("\n Addition:",calculation.add(a,b))
+print("\n Subtraction:",calculation.sub(a,b))
+print("\n Multiplication",calculation.mult(a,b))
+print("\n Division",calculation.div(a,b))
