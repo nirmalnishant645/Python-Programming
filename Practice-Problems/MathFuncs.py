4e5cbd83367f7a5775a042254f1a6a20a59b7645
nirmalnishant645 <nirmalnishant645@gmail.com>
2019-10-19 00:31:05 +0530

    Five Math Functions

---
+++ Practice-Problems/MathFuncs.py
@@ -0,0 +1,7 @@
+import math
+
+print ("The absolute value of -7 is :",math.fabs(-7))
+print ("The factorial of 5 is : ",math.factorial(5))
+print ("The greatest common divisor of 60 and 72 is : ",math.gcd(60,72))
+print ("The smallest integral value greater than the number of 6.7 is : ",math.ceil(6.7))
+print ("The greatest integral value smaller than the number of 6.7 is : ",math.floor(6.7)) 
