74d4e1f14545135056857e5609789bc26d692a64
nirmalnishant645 <nirmalnishant645@gmail.com>
2019-12-14 21:10:54 +0530

    Linear Search

--- Data-Structures-and-Algorithms/LinearSearch.py
+++ Data-Structures-and-Algorithms/LinearSearch.py
@@ -9,3 +9,5 @@ def LinearSearch(arr, target):
 #Best Case, target at arr[1], loop runs once, Time Complexity O(1)
 #Worst Case, target at arr[n], loop runs n times, Time Complexity O(n)
 #Average Case, target at arr[2]-arr[n-1], loop runs at least twice or at most n-2 times, Time Complexity remains O(n) (Constant not considered, n/2 changes to n)
+
+#Since we are not using any extra data structure therefore the space complexity will be O(1)
