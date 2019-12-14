a284ec0680f5e4179f1a809d419bfcbf57673b25
nirmalnishant645 <nirmalnishant645@gmail.com>
2019-12-14 13:37:47 +0530

    Linear Search

---
+++ Data-Structures-and-Algorithms/LinearSearch.py
@@ -0,0 +1,11 @@
+#Linear Search Algorithm in List
+
+def LinearSearch(arr, target):
+    for num in arr:
+        if num == target:
+            return True
+    return False
+
+#Best Case, target at arr[1], loop runs once, Time Complexity O(1)
+#Worst Case, target at arr[n], loop runs n times, Time Complexity O(n)
+#Average Case, target at arr[2]-arr[n-1], loop runs at least twice or at most n-2 times, Time Complexity remains O(n) (Constant not considered, n/2 changes to n)
