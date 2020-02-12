d3639a96d9342e9f0246e628c99158d37ade721e
Nishant Nirmal <nirmalnishant645@gmail.com>
2020-02-12 10:59:40 +0530

    Value equal to Index

--- Practice-Problems/Index-Value.py
+++ Practice-Problems/Index-Value.py
@@ -5,12 +5,18 @@ Otherwise, return False.
 For example, given [-6, 0, 2, 40], you should return 2. Given [1, 5, 7, 8], you should return False.
 '''
 
-#Time Complexity - O(n), Space Complexity - O(1)
+#Time Complexity - O(log n), Space Complexity - O(1)
 
 def indexValue(arr):
-    for i in range(len(arr)):
-        if i == arr[i]:
-            return i
+    first, last = 0, len(arr) - 1
+    while first <= last:
+        mid = (first + last) // 2
+        if mid == arr[mid]:
+            return mid
+        if arr[mid] < mid:
+            first = mid + 1
+        else:
+            last = mid - 1
     return False
 
 arr = list(map(int, input().split()))
