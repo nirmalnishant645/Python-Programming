9d7d43835c25731a9b0bfca41dea7aeca2fc51e1
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-06-09 15:28:51 +0530

    Anagram Check

--- Practice-Problems/AnagramCheck.py
+++ Practice-Problems/AnagramCheck.py
@@ -19,3 +19,34 @@ def anagram(s1, s2):
     s2 = s2.replace(' ','').lower()
 
     return sorted(s1) == sorted(s2)
+
+# Method 2, Hashing
+
+def anagram(s1, s2):
+
+    s1 = s1.replace(' ','').lower()
+    s2 = s2.replace(' ','').lower()
+
+    #Edge Case Check
+    if len(s1) != len(s2):
+        return False
+
+    count = {}
+
+    for letter in s1:
+        if letter in count:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+
+    for letter in s2:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0:
+            return False
+
+    return True
