828a6d1a01583e5a89d402d62219e180ce688e0b
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-06-09 15:28:04 +0530

    Anagram Check

---
+++ Practice-Problems/AnagramCheck.py
@@ -0,0 +1,13 @@
+'''
+Given two strings, check to see if they are anagrams.
+An anagram is when two strings can be written using the exact same letters.
+(So you can just rearrange the letters to get a different phrase or word)
+
+For example:
+
+"public relations" is an anagram of "crap built on lies"
+"clint eastwood" is an anagram of "old west action"
+
+NOTE:
+Ignore spaces and capitalization. So "d go" is and anagram for "God" and "dog" and "o d g".
+'''
