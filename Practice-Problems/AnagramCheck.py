aa1b097b3c9a662e70a605230671ea110554d7f8
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-06-09 15:28:26 +0530

    Anagram Check

--- Practice-Problems/AnagramCheck.py
+++ Practice-Problems/AnagramCheck.py
@@ -11,3 +11,11 @@ For example:
 NOTE:
 Ignore spaces and capitalization. So "d go" is and anagram for "God" and "dog" and "o d g".
 '''
+# Method 1 (NOT OPTIMAL)
+
+def anagram(s1, s2):
+
+    s1 = s1.replace(' ','').lower()
+    s2 = s2.replace(' ','').lower()
+
+    return sorted(s1) == sorted(s2)
