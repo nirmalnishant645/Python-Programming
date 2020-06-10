6b1765a3c928d58dcf13c0dcf54f86b025da2ad4
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-06-10 16:02:57 +0530

    Array Pair Sum

--- Practice-Problems/ArrayPairSum.py
+++ Practice-Problems/ArrayPairSum.py
@@ -13,3 +13,26 @@ would return 2 pairs:
 NOTE:
 For testing purposes change your function so it outputs the number of pairs
 '''
+
+def pair_sum(arr, k):
+
+    if len(arr) < 2:
+        return
+
+    #Sets for tracking
+    seen = set()
+    output = set()
+
+    for num in arr:
+
+        target = k - num
+
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add(((min(num, target)), max(num, target)))
+
+    return len(output)
+
+
+print(pair_sum([1,3,2,2], 4))
