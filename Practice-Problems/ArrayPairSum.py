775975082660e925992966dda23f47a26930efc3
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-06-10 15:55:39 +0530

    Array Pair Sum

---
+++ Practice-Problems/ArrayPairSum.py
@@ -0,0 +1,15 @@
+'''
+Given an integer array, output all the unique pairs that sum up to a specific value k.
+
+So the input:
+
+    pair_sum([1, 3, 2, 2], 4)
+
+would return 2 pairs:
+
+    (1,3)
+    (2,2)
+
+NOTE:
+For testing purposes change your function so it outputs the number of pairs
+'''
