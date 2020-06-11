f65d3f70dfda8a7b45275c201d16390ddf88a646
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-06-11 16:39:03 +0530

    Missing Element

--- Practice-Problems/FindTheMissingElement.py
+++ Practice-Problems/FindTheMissingElement.py
@@ -13,3 +13,15 @@ Output:
 
     5 is the missing number.
 '''
+
+def finder(arr1, arr2):
+
+    arr1.sort()
+    arr2.sort()
+
+    for num1, num2 in zip(arr1, arr2):
+        if num1 != num2:
+            return num1
+    return arr1[-1]
+
+print(finder([1,2,3,4,5,6,7], [3,7,2,1,4,6]))
