285673e3f2099b1c171e516c84aff08034e09a20
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-06-13 18:30:18 +0530

    Largest Continuous Sum

--- Practice-Problems/LargestContinuousSum.py
+++ Practice-Problems/LargestContinuousSum.py
@@ -1,3 +1,17 @@
 '''
 Given an array of integers (positive and negative) find the largest continuous sum.
 '''
+
+def large_cont_sum(arr):
+
+    if not len(arr):
+        return None
+
+    max_sum = cur_sum = arr[0]
+
+    for num in arr[1:]:
+
+        cur_sum = max(cur_sum + num, num)
+        max_sum = max(cur_sum, max_sum)
+
+    return max_sum
