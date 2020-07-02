734c45c09cca1af38792416b444094f58ebd7a8a
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-02 12:21:27 +0530

    Sentence Reversal

--- Practice-Problems/SentenceReversal.py
+++ Practice-Problems/SentenceReversal.py
@@ -12,3 +12,22 @@ As a part of this exercise you should remove all leading and trailing whitespace
 both become:
     'here space'
 '''
+def rev_word(s):
+    words = []
+    length = len(s)
+    space = [' ']
+
+    i = 0
+
+    while i < length:
+        if s[i] not in space:
+            word_start = i
+
+            while i < length and s[i] not in space:
+                i += 1
+            
+            words.append(s[word_start:i])
+
+        i += 1
+
+    return " ".join(reversed(words))
