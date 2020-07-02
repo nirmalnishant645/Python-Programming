32538632427c54f10c84352b91e79eca54cab4b7
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-02 12:21:07 +0530

    Sentence Reversal

---
+++ Practice-Problems/SentenceReversal.py
@@ -0,0 +1,14 @@
+'''
+Given a string of words, reverse all the words. For Example:
+
+Given:
+    'This is the best'
+
+Return:
+    'Best the is This'
+
+As a part of this exercise you should remove all leading and trailing whitespace. So that inputs such as:
+    '    space here' and 'space here    '
+both become:
+    'here space'
+'''
