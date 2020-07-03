19276a913d99f8a44680f16da676bb6ccb0c1fb0
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-03 12:37:38 +0530

    String Compression

---
+++ Practice-Problems/StringCompression.py
@@ -0,0 +1,6 @@
+'''
+Given a string in the form 'AAAABBBBCCCCCDDEEEE' compress it to become 'A4B4C5D2E4'.
+For this problem, you can falsely "compress" strings of single or double letters. For instance, it is ok for 'AAB' to return 'A2B1' even though this technically takes more space.
+
+The function should also be case sensitive, so that a string 'AAAaaa' returns 'A3a3'.
+'''
