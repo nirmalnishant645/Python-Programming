70be1fb265886866f8e3955345c05848851f6ddb
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-03 12:38:02 +0530

    String Compression

--- Practice-Problems/StringCompression.py
+++ Practice-Problems/StringCompression.py
@@ -4,3 +4,30 @@ For this problem, you can falsely "compress" strings of single or double letters
 
 The function should also be case sensitive, so that a string 'AAAaaa' returns 'A3a3'.
 '''
+
+def compress(s):
+    
+    r = ""
+    l = len(s)
+
+    if not l:
+        return ""
+    if l == 1:
+        return s+"1"
+
+    last = s[0]
+    count = 1
+    i = 1
+
+    while i < l:
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            r = r + s[i - 1] + str(count)
+            count = 1
+        
+        i += 1
+
+    r = r + s[i - 1] + str(count)
+
+    return r
