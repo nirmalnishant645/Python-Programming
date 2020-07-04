5b001f46eaa70bb345ce5ce96cfcd666e6a7a975
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-04 12:48:22 +0530

    Unique Character

--- Practice-Problems/UniqueCharacterInString.py
+++ Practice-Problems/UniqueCharacterInString.py
@@ -8,3 +8,16 @@ The string 'aabcde' contains duplicate characters and should return False.
 
 def uni_char(s):
     return len(set(s)) == len(s)
+
+# Using only Built-In Data Structure
+
+def uniq_char(s):
+
+    chars = set()
+
+    for letter in s:
+        if letter in chars:
+            return False
+        else:
+            chars.add(letter)
+    return True
