0618b9bb96b09a8a6aa8f69f5ed285bbb76a7f90
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-04 12:47:44 +0530

    Unique Character

---
+++ Practice-Problems/UniqueCharacterInString.py
@@ -0,0 +1,5 @@
+'''
+Given a string, determine if it has all unique characters.
+For example, the string 'abcde' has all unique characters and should return True.
+The string 'aabcde' contains duplicate characters and should return False.
+'''
