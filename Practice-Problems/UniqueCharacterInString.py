67f9990f69d2abd34fefe3dabbe1583e643cab5c
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-04 12:48:03 +0530

    Unique Character

--- Practice-Problems/UniqueCharacterInString.py
+++ Practice-Problems/UniqueCharacterInString.py
@@ -3,3 +3,8 @@ Given a string, determine if it has all unique characters.
 For example, the string 'abcde' has all unique characters and should return True.
 The string 'aabcde' contains duplicate characters and should return False.
 '''
+
+# (One - Liner) Using Built-In Data Structure and Built-In Function
+
+def uni_char(s):
+    return len(set(s)) == len(s)
