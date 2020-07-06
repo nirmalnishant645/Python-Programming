99a179d84d97d66211882f4e307a4f103e4517ed
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-06 20:30:31 +0530

    Stack Implementation

--- Data-Structures-and-Algorithms/Stack.py
+++ Data-Structures-and-Algorithms/Stack.py
@@ -10,48 +10,24 @@ The current page that you are viewing is on the top and the first page you looke
 If you click on the Back Button, you begin to move in reverse order through pages.
 '''
 
-class Stack:
-    #Initializing a list
-    arr = []
+# Stack() creates a new stack that is empty.
+# It needs no parameters and returns an empty stack.
+class Stack(object):
 
-    #To push an element into the Stack
-    def push(self, item):
-        self.arr.append(item)
+    def __init__(self):
+        self.items = []
 
-    #To check if the stack is empty
-    def isEmpty(self):
-        return not self.arr
+    def isEmpty(self):  # Tests to see whether the stack is empty.
+        return not self.items   # It needs no parameters and returns an integer.
 
-    #To pop the last element off the stack
-    def pop(self):
-        return None if self.isEmpty() else self.arr.pop()
+    def push(self, item):   # Adds a new item to the top of the stack.
+        self.items.append(item) # It needs the item and returns nothing.
 
-    #To peek into the Stack
-    def peek(self):
-        return None if self.isEmpty() else self.arr[-1]
+    def pop(self):  # Removes the top item from the stack.
+        return self.items.pop() # It needs no parameters and returns the item. Stack is modified.
 
+    def peek(self): # Returns the top item from the stack but does not remove it.
+        return None if self.isEmpty() else self.items[len(self.items) - 1]  # It needs no parameters. The stack is not modified.
 
-    #To display the whole Stack
-    def display(self):
-        print(self.arr)
-
-x = Stack() #Creating an object of stack class
-x.display()
-print('Pushing 1 into the stack')
-x.push('1')
-x.display()
-print('Pushing 2 into the stack')
-x.push('2')
-x.display()
-print('Peeking into the top of the stack')
-print(x.peek())
-print('Popping the last element from the stack')
-print(x.pop())
-x.display()
-print('Popping the last element from the stack')
-print(x.pop())
-x.display()
-print('Checking if the stack is empty')
-print(x.isEmpty())
-print('Popping the stack even though it is empty')
-print(x.pop())
+    def size(self): # Returns the number of items on the stack.
+        return len(self.items)  # It needs no parameters and returns an integer.
