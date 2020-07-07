60fc564e50b07946630cf85989ef3729985a37c3
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-07 20:59:27 +0530

    Queue Implementation

--- Data-Structures-and-Algorithms/Queue.py
+++ Data-Structures-and-Algorithms/Queue.py
@@ -4,25 +4,27 @@ ENQUEUE - Adding a new item to the rear of the queue.
 DEQUEUE - Removing the front item from the queue.
 '''
 
+# Queue() creates a new queue that is empty.
+# It needs no parameters and returns an empty queue.
 class Queue(object):
 
     def __init__(self):
         self.items = []
 
-    def isEmpty(self):
-        return not self.items
+    def isEmpty(self):  # Tests to see whether the queue is empty.
+        return not self.items   # It needs no parameters and returns a boolean value.
 
-    def enqueue(self, item):
-        return self.items.append(item)
+    def enqueue(self, item):    # Adds a new item to the rear of the queue.
+        return self.items.append(item)  # It needs the item and returns nothing.
 
-    def dequeue(self):
-        return None if self.isEmpty() else self.items.pop(0)
+    def dequeue(self):  # Removes the front item from the queue.
+        return None if self.isEmpty() else self.items.pop(0)    # It needs no parameters and returns the item. Queue is modified.
 
-    def size(self):
-        return len(self.items)
+    def size(self): # Returns the number of items in the queue.
+        return len(self.items)  # It needs no parameters and returns an integer value.
 
-    def front(self):
-        return None if self.isEmpty else self.items[0]
+    def front(self):    # Returns the front item from the stack but does not remove it.
+        return None if self.isEmpty else self.items[0]  # It needs no parameters. The stack is not modified.
 
-    def rear(self):
-        return None if self.isEmplty else self.items[-1]
+    def rear(self): # Returns the rear item from the stack but does not remove it.
+        return None if self.isEmpty else self.items[-1]    # It needs no parameters. The stack is not modified.
