4f2844674669cbfeebfaca04711aa44c6d0bdec1
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-08 15:56:54 +0530

    Balanced Parentheses

---
+++ Practice-Problems/BalancedParenthesesCheck.py
@@ -0,0 +1,12 @@
+'''
+Given a string of opening and closing parentheses, check whether it's balanced.
+We have 3 types of parentheses: 
+    Round Brackets ()
+    Square Brackets []
+    Curly Brackets {}
+Assume that the string doesn't contain any other character than these, no spaces, words or numbers.
+As a reminder, balanced parentheses require every opening parentheses to be closed in the reversed order opened.
+For example, '([])' is balanced but '([)]' is not.
+
+You can assume the input string has no spaces.
+'''
