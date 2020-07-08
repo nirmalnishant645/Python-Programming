4e6c1eca17e08c49a36c79cdbd30e8f103ab3946
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-08 19:25:23 +0530

    Implement Queue using two stacks

---
+++ Practice-Problems/ImplementQueueUsingTwoStacks.py
@@ -0,0 +1,18 @@
+'''
+Given the Stack class below, implement a Queue using two stacks.
+'''
+
+class Queue2Stacks(object):
+
+    def __init__(self):
+
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, element):
+
+        pass
+
+    def dequeue(self, element):
+
+        pass
