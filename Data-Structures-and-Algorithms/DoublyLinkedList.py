410e69fe1c99915c8c5f71080e5b4df40e6dcd4c
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-10 15:54:29 +0530

    Doubly Linked List

---
+++ Data-Structures-and-Algorithms/DoublyLinkedList.py
@@ -0,0 +1,11 @@
+'''
+Doubly Linked List is a variation of Linked list in which navigation is possible in both ways,
+either forward and backward easily as compared to Single Linked List.
+Each node stores a reference to an object that is an element of the sequence, and  a reference to the next node,
+just like in the case of singly linked list, but in this case, it will also store a reference to the previous node.
+This allows a greater variety of O(1)-time update operations.
+List also maintains two members called 'head' and 'tail' that identifies the first and the last node of the list respectively.
+Next or Prev reference of a node can be thought of as a link or a pointer to another node.
+
+* It does not have predetermined fixed size.
+'''
