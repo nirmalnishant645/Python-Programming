1ef2cfd78549992d8faa99fa977dcd8ace2e5886
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-14 00:36:30 +0530

    Coin Change Problem

--- Practice-Problems/CoinChangeProblem.py
+++ Practice-Problems/CoinChangeProblem.py
@@ -16,3 +16,27 @@ If n = 10 and coins = [1,5,10]. Then there are 4 possible ways to make change:
 
 With 1 coin being the minimum amount.
 '''
+
+# Basic Recursion (Not Optimal)
+def rec_coin(target, coins):
+
+    # Default Value set to target
+    min_coins = target
+
+    # Base case to check if target is in coin values list 
+    if target in coins:
+
+        return 1
+
+    else:
+
+        # For every coin value that is <= target
+        for i in [c for c in coins if c <= target]:
+
+            # Add a coin count + Recursive  
+            num_coins = 1 + rec_coin(target - i, coins)
+
+            # Reset minimum if new num_coins < min_coins
+            min_coins = min(num_coins, min_coins)
+
+    return min_coins
