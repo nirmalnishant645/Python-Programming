0d47186a774777deaabe7e9b927a902b5ed5cf31
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-14 00:36:10 +0530

    Coin Change

---
+++ Practice-Problems/CoinChangeProblem.py
@@ -0,0 +1,18 @@
+'''
+Given a target amount n and a list (array) of distinct coin values,
+what's the fewest coins needed to make the change amount.
+
+For example:
+
+If n = 10 and coins = [1,5,10]. Then there are 4 possible ways to make change:
+
+1+1+1+1+1+1+1+1+1+1
+
+5 + 1+1+1+1+1
+
+5+5
+
+10
+
+With 1 coin being the minimum amount.
+'''
