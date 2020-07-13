e6a3b84578bfb298bd57b82986f2c23cc4183fd4
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-07-13 13:52:56 +0530

    Fibonnaci Sequence

--- Practice-Problems/FibonnaciSequence.py
+++ Practice-Problems/FibonnaciSequence.py
@@ -12,3 +12,14 @@ Remember that a fibonacci sequence: 0,1,1,2,3,5,8,13,21,...
 starts off with a base case checking to see if n = 0 or 1, then it returns 1.
 Else it returns fib(n-1)+fib(n-2).
 '''
+
+# Iteratively
+def fib_iter(n):
+
+    res, next = 0, 1
+
+    for i in range(n):
+
+        res, next = next, res + next
+
+    return res
