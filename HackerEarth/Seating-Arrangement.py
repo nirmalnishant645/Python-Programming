01d8a33ac75025b54056e193a4cabdacd9731035
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-10-17 08:08:18 +0530

    Seating Arrangement

---
+++ HackerEarth/Seating-Arrangement.py
@@ -0,0 +1,29 @@
+'''
+Akash and Vishal are quite fond of travelling.
+They mostly travel by railways.
+They were travelling in a train one day and they got interested in the seating arrangement of their compartment.
+The compartment looked something like
+
+So they got interested to know the seat number facing them and the seat type facing them.
+The seats are denoted as follows :
+
+Window Seat : WS
+Middle Seat : MS
+Aisle Seat : AS
+
+You will be given a seat number,
+find out the seat number facing you and the seat type,
+i.e. WS, MS or AS.
+
+INPUT
+First line of input will consist of a single integer T denoting number of test-cases.
+Each test-case consists of a single integer N denoting the seat-number.
+
+OUTPUT
+For each test case, print the facing seat-number and the seat-type,
+separated by a single space in a new line.
+
+CONSTRAINTS
+1<=T<=105
+1<=N<=108
+'''
