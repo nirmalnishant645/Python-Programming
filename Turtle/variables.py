b1fbef2d678b663e04dddc23dcc453aca84bd914
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-10-23 11:28:39 +0530

    variable with turtle

---
+++ Turtle/variables.py
@@ -0,0 +1,14 @@
+import turtle
+
+# To make a square
+test_turtle = turtle.Turtle()
+test_turtle.forward(100)
+test_turtle.right(90)
+test_turtle.forward(100)
+test_turtle.right(90)
+test_turtle.forward(100)
+test_turtle.right(90)
+test_turtle.forward(100)
+
+#To stop tkinter window from ending itself on its own
+turtle.exitonclick()
