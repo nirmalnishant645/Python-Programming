bea26af7ebe53f6cc4b20e5599c2a1d935579b2d
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-10-28 15:53:09 +0530

    Tuples

--- Variables-DataTypes-and-Functions/Tuples.py
+++ Variables-DataTypes-and-Functions/Tuples.py
@@ -5,3 +5,15 @@ tuple = ("Hello", 3, 4.6)
 print(tuple)
 print("Data Type of an item in Tuple:",type(tuple[2]))
 print("Print the last element of the tuple:",tuple[-1])
+print()
+
+person_1 = ('Nishant', 25, 'Computer')
+person_2 = ('Kunal', 23, 'Camera')
+
+people = [person_1, person_2]
+
+for name, age, tool in people:
+    print(name)
+    print(age)
+    print(tool)
+    print()
