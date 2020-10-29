48721862571dbeb1a44b6fe23821032791fe9d15
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-10-29 15:53:09 +0530

    Dictionaries

--- Variables-DataTypes-and-Functions/Dictionaries.py
+++ Variables-DataTypes-and-Functions/Dictionaries.py
@@ -7,3 +7,18 @@ print(a["First_Name"])
 print(a["Cities"])
 '''Accesing a nested item (Item inside a List or Tuple)'''
 print(a["Cities"][2])
+
+
+contacts = {
+    'Nishant' : {'Phone' : '8563782018', 'Email' : 'nirmalnishant645@gmail.com'},
+    'Sachin' : {'Phone' : '6472839201', 'Email' : 'sachin_t@yahoo.co.in'},
+    'Roger' : {'Phone' : '7485930492', 'Email' : 'rf_007@rediff.co.in'}
+}
+
+print(contacts['Nishant']['Phone'])
+print(contacts['Sachin'])
+print(contacts['Roger']['Email'])
+
+print(contacts.items())
+print(contacts.keys())
+print(contacts.values())
