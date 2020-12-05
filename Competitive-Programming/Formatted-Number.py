bc4ed4e19bc07de8e90294e0cc37a8ead301454f
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-06 00:57:16 +0530

    Formatted Numbers

---
+++ Competitive-Programming/Formatted-Number.py
@@ -0,0 +1,16 @@
+'''
+Have the function FormattedNumber(strArr) take the strArr parameter being passed,
+which will only contain a single element,
+and return the string true if it is a valid number that contains only digits with properly placed decimals and commas,
+otherwise return the string false.
+For example: if strArr is ["1,093,222.04"] then your program should return the string true,
+but if the input were ["1,093,22.04"] then your program should return the string false.
+The input may contain characters other than digits.
+
+Examples
+Input: ["0.232567"]
+Output: true
+
+Input: ["2,567.00.2"]
+Output: false
+'''
