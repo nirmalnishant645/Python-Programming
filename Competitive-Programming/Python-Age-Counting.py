028b02cb2ecbb9f43f63bee33b7b40d0f8f5c144
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-07 00:58:24 +0530

    Python Age Counting

---
+++ Competitive-Programming/Python-Age-Counting.py
@@ -0,0 +1,11 @@
+'''
+In the Python file, write a program to perform a GET request on the route https://coderbyte.com/api/challenges/json/age-counting
+which contains a data key and the value is a string which contains items in the format:
+key=STRING, age=INTEGER. Your goal is to count how many items exist that have an age equal to or greater than 50, and print this final value.
+
+Example Input
+{"data":"key=IAfpK, age=58, key=WNVdi, age=64, key=jp9zt, age=47"}
+
+Example Output
+2
+'''
