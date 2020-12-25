1cf69b5cdc26bcf874c69c9ac8964f74ccc01fb1
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-25 19:33:28 +0530

    Directed Graph using Adjacency List

---
+++ Data-Structures-and-Algorithms/Directed-Graph-using-Adjacency-List.py
@@ -0,0 +1,5 @@
+'''
+An array/dictionary of lists is used. Size of the array/dictionary is equal to the number of vertices. Let the array be array[]. 
+An entry array[i] represents the list of vertices adjacent to the ith vertex. This representation can also be used to represent a weighted graph. 
+The weights of edges can be represented as lists of pairs.
+'''
