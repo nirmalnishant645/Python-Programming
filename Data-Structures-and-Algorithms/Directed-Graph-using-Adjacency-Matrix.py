8490a3a3b5922fe4e332229e6d99428ee7a6fdb5
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-26 19:35:27 +0530

    Directed Graph using Adjacency Matrix

---
+++ Data-Structures-and-Algorithms/Directed-Graph-using-Adjacency-Matrix.py
@@ -0,0 +1,19 @@
+# Simple Graph Implementation
+
+class Graph:
+    def __init__(self, number_of_nodes): # Initialise Graph Data Structure
+        self.number_of_nodes = number_of_nodes + 1 # Number of nodes will have to be increased by one 
+        self.graph = [[0 for x in range(number_of_nodes + 1)] for y in range(number_of_nodes + 1)]
+
+    def withInBounds(self, v1, v2): # Function to check if the value is within matrix bounds
+        return v1 >= 0 and v1 <= self.number_of_nodes and v2 >= 0 and v2 <=  self.number_of_nodes
+
+    def insertEdge(self, v1, v2): # Function to inser edge in the Graph
+        if self.withInBounds(v1, v2): # Check if values are within bounds
+            self.graph[v1][v2] = 1 # Change the value of the node from 0 to 1
+
+    def printGraph(self): # Functipon to Print Graph
+        for i in range(self.number_of_nodes):
+            for j in range(len(self.graph[i])):
+                if self.graph[i][j]:
+                    print(i, '=>', j)
