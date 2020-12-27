c6831996e0c3e9cf3dd123cb3f54cbe026bb1c0d
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-27 19:36:27 +0530

    Undirected Graph using Adjacency List

---
+++ Data-Structures-and-Algorithms/Undirected-Graph-using-Adjacency-List.py
@@ -0,0 +1,9 @@
+'''
+An array/dictionary of lists is used. Size of the array/dictionary is equal to the number of vertices. Let the array be array[]. 
+An entry array[i] represents the list of vertices adjacent to the ith vertex. This representation can also be used to represent a weighted graph. 
+The weights of edges can be represented as lists of pairs.
+
+The Implementation will be same as Directed Graph, but this time the connection will be done both ways.
+If v1 => v2 then v2 => v1
+Or v1 <=> v2
+'''
