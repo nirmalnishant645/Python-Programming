6025882df8f3ac76205317204add35950d0d2e7c
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-27 19:36:42 +0530

    Graph Implementation

--- Data-Structures-and-Algorithms/Undirected-Graph-using-Adjacency-List.py
+++ Data-Structures-and-Algorithms/Undirected-Graph-using-Adjacency-List.py
@@ -7,3 +7,25 @@ The Implementation will be same as Directed Graph, but this time the connection 
 If v1 => v2 then v2 => v1
 Or v1 <=> v2
 '''
+
+# Simple Directed Graph
+
+class Graph:
+    def __init__(self): # Initialise Graph
+        self.graph = {}
+
+    def insertEdge(self, val1, val2): # Insert Edge in the Graph
+        if val1 in self.graph: # Check if vertex already exists
+            self.graph[val1].append(val2) # Append the list directed by the vertex
+        else:
+            self.graph[val1] = [val2] # Add new vertex  if doesn't exist
+
+        if val2 in self.graph: # Additional Connection for Undirected Graph (Or directing both ways v1 <=> v2)
+            self.graph[val2].append(val1)
+        else:
+            self.graph[val2] = [val1]
+
+    def  printGraph(self): # Print Graph
+        for node in self.graph:
+            for val in self.graph[node]:
+                print(node, ' => ', val) # Loop through the dictionary and print each node and the node that it is pointing to
