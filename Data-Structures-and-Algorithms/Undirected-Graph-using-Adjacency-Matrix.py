4cde96a20e0a75e7c493f4c3f37f42f4cdaab171
nirmalnishant645 <nirmalnishant645@gmail.com>
2020-12-28 19:37:44 +0530

    Graph Implementation

--- Data-Structures-and-Algorithms/Undirected-Graph-using-Adjacency-Matrix.py
+++ Data-Structures-and-Algorithms/Undirected-Graph-using-Adjacency-Matrix.py
@@ -3,3 +3,24 @@ The Implementation will be same as Directed Graph, but this time the connection 
 If v1 => v2 then v2 => v1
 Or v1 <=> v2
 '''
+
+# Simple Graph Implementation
+
+class Graph:
+    def __init__(self, number_of_nodes): # Initialise Graph Data Structure
+        self.number_of_nodes = number_of_nodes + 1 # Number of nodes will have to be increased by one 
+        self.graph = [[0 for x in range(number_of_nodes + 1)] for y in range(number_of_nodes + 1)]
+
+    def withInBounds(self, v1, v2): # Function to check if the value is within matrix bounds
+        return v1 >= 0 and v1 <= self.number_of_nodes and v2 >= 0 and v2 <=  self.number_of_nodes
+
+    def insertEdge(self, v1, v2): # Function to inser edge in the Graph
+        if self.withInBounds(v1, v2): # Check if values are within bounds
+            self.graph[v1][v2] = 1 # Change the value of the node from 0 to 1
+            self.graph[v2][v1] = 1 # Adding the two way relation between the vertices to make in undirected
+
+    def printGraph(self): # Functipon to Print Graph
+        for i in range(self.number_of_nodes):
+            for j in range(len(self.graph[i])):
+                if self.graph[i][j]:
+                    print(i, '=>', j)
