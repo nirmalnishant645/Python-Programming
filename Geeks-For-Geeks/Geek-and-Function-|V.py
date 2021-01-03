09ed08fef755169dfeadbed5ca1f43043e926e75
nirmalnishant645 <nirmalnishant645@gmail.com>
2021-01-03 18:45:46 +0530

    GFG Geek and Function

---
+++ Geeks-For-Geeks/Geek-and-Function-|V.py
@@ -0,0 +1,26 @@
+'''
+Given two integers numbers N and K, the Geek wants you to find f(f(..........f(N))) K times, where f(x) = x XOR (x%10).
+Note: XOR represents bitwise xor operation
+
+Input:
+1. The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+2. The first line of each test case contains two integers N and K.
+
+Output: For each test case, print the answer
+
+Constraints:
+1. 1 <= T <= 10
+2. 1 <= N <= 105
+3. 1 <= K <= 109
+
+Example:
+
+Input:
+2
+17 1
+66 3
+
+Output:
+22
+74
+'''
