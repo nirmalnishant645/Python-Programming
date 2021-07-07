0a430ad28afa4870eb18d9abcda33764372eac53
Nishant Roche <nirmalnishant645@gmail.com>
2021-07-07 15:52:20 +0530

    Webhook Server

--- Webhooks/server.py
+++ Webhooks/server.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, abort
+import requests
+import json
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST', 'GET'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == 'GET':
-        return '<h1>Success!<h1>'
     if request.method == 'POST':
-        print(request.json)
+        print('hello2')
+        data = request.json
+        param = {"sid" : data["sid"],
+        "search_name" : data["search_name"],
+        "owner" : data["owner"],
+        "results_link" : data["results_link"],
+        "result" : data["result"]}
+        webhook_url = "https://hooks.slack.com/services/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"
+        # r = requests.post(webhook_url, data = {"text":param["owner"]}, headers={'Content-Type': 'application/json'})  # Sending data using Post() request as webhook
+        print(param)
         return 'success', 200
     else:
         abort(400)
